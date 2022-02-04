3065e8f34481ccaf6e810967cd9235c1b355e7c0
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-02-04 18:30:35 +0200

    Update task 10 after check

--- js/task-10.js
+++ js/task-10.js
@@ -4,7 +4,12 @@ function getRandomHexColor() {
 
 function createBoxes(input) {
   for (let i = 0; i < input.value; i += 1) {
-    const increaseDiv = 30 + i * 10;
+
+    const styleLastElement = (divBoxes.lastElementChild !== null) ? divBoxes.lastElementChild.getAttribute('style') : 'not';
+    const heightLastElement = (styleLastElement === 'not') ? 30 : Number.parseInt(styleLastElement.slice(7, styleLastElement.length)) + 10;
+    const heightActual = (30 > heightLastElement) ? 30 : heightLastElement;
+    
+    const increaseDiv = heightActual + i * 10;
     const randomColor = getRandomHexColor();
     const markup = `<div style="height: ${increaseDiv}px; width: ${increaseDiv}px; background-color: ${randomColor}"></div>`;
     divBoxes.insertAdjacentHTML("beforeend", markup);
